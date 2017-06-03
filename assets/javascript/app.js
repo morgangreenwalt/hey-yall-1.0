987d967dc3bf28a6a3999e60c794047e475845eb
Tin Nguyen <Tinman@Tins-MBP.lan>
2017-06-03 12:58:20 -0500

    updating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,30 +7,65 @@ var eventsArray = [];
 // document.getElementById("defaultOpen").click();
 function food() {
 	$(".food").on("click", function(){
-		var selection = $(this).attr("data-name");
-		foodArray.push(selection);
+		if($(this).attr("data-state")=="unclicked"){
+			var selection = $(this).attr("data-name");
+			foodArray.push(selection);
 
-		$(this).css("border", "5px solid #FB7722");
-	    console.log(foodArray);
+			$(this).addClass("clicked");
+			$(this).attr("data-state","clicked");
+		}
+		else if ($(this).attr("data-state")=="clicked"){
+			var selection = $(this).attr("data-name");
+			var index = foodArray.indexOf(selection);
+			foodArray.splice(index, 1);
+
+			$(this).removeClass("clicked");
+			$(this).attr("data-state","unclicked");
+		}
+		console.log(foodArray);
 	});
 }
 
 function drinks() {
 	$(".drinks").on("click", function(){
-		var selection = $(this).attr("data-name");
-		drinksArray.push(selection);
+		if($(this).attr("data-state")=="unclicked"){
+			if(drinksArray.length<2){
+				var selection = $(this).attr("data-name");
+				drinksArray.push(selection);
 
-		$(this).css("border", "5px solid #FB7722");
+				$(this).addClass("clicked");
+				$(this).attr("data-state","clicked");
+			}
+		}
+		else if ($(this).attr("data-state")=="clicked"){
+			var selection = $(this).attr("data-name");
+			var index = foodArray.indexOf(selection);
+			drinksArray.splice(index, 1);
+			
+			$(this).removeClass("clicked");
+			$(this).attr("data-state","unclicked");
+		}
 	    console.log(drinksArray);
 	});
 }
 
 function events() {
 	$(".events").on("click", function(){
-		var selection = $(this).attr("data-name");
-		eventsArray.push(selection);
+		if($(this).attr("data-state")=="unclicked"){
+			var selection = $(this).attr("data-name");
+			eventsArray.push(selection);
 
-		$(this).css("border", "5px solid #FB7722");
+			$(this).addClass("clicked");
+			$(this).attr("data-state","clicked");
+		}
+		else if ($(this).attr("data-state")=="clicked"){
+			var selection = $(this).attr("data-name");
+			var index = foodArray.indexOf(selection);
+			eventsArray.splice(index, 1);
+			
+			$(this).removeClass("clicked");
+			$(this).attr("data-state","unclicked");
+		}
 	    console.log(eventsArray);
 	});
 }
@@ -41,3 +76,11 @@ events();
 
 
 });
+
+
+
+
+
+
+
+
