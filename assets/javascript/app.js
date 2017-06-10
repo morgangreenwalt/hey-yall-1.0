e33363af70cd504a8ad3189454a4c430af4c9943
morgan greenwalt <you@example.com>
2017-06-10 09:55:09 -0500

    edits morgan

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -774,11 +774,66 @@ $.ajax({
 
 
 
+function googleSignIntwo() {
+    firebase.auth().signInWithPopup(google).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
 
+        loadProfilePage()
+            
+        }).catch(function(error) {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // The email of the user's account used.
+              var email = error.email;
+              // The firebase.auth.AuthCredential type that was used.
+              var credential = error.credential;
+              // ...
+            });
+ }
 
+function facebookSignInTwo() {
+    firebase.auth().signInWithPopup(facebook).then(function(result) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+        
+         
+         loadProfilePage()
+        
+       }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+ }
 
+$(".test").on("click", function(event) {
+   event.preventDefault()
+    var method = $(this).attr("data")
+    console.log("hello")
+       if (method === "googleLogin") {
+        googleSignIntwo();
+        
+       }
+    else if(method === "facebookLogin") {
+        facebookSignInTwo()
+        
+   }
+})
 
-
+function loadProfilePage() {
+     window.location.href= "login.html"
+}
 
 
 $(".devs").hover(function(){
